d45627171db340b4c5d7a04fb61a37e62653201b
islemamirhassen <islem.ahmed92@gmail.com>
2024-11-14 18:03:36 +0100

    modified

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,6 @@
 const listeMots=["Cachalot", "Pétunia","Serviette"];
 let score=0;
-let motUtilisateur=prompt("entre le mot:"+listeMots[0])
+let motUtilisateur=prompt("entre le mot:" +listeMots[0])
 
 if(motUtilisateur===listeMots[0]){
     score++
